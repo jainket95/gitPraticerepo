81d03bf25a035854726da1f912e3044530e7072b
thelucidrealist <aniket1995jain@gmail.com>
2019-07-09 16:26:28 +0530

    branch changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
        But I don't know why does my git add command is not working, may be this is already added to the git repo.
        Now lets work on commit 1
        Now lets work on commit 2
+       new branch nigga
       </header>
     </div>
   );
