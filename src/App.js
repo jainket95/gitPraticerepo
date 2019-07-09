7de911f20297565bd2c36e595fa4764b54781ea1
thelucidrealist <aniket1995jain@gmail.com>
2019-07-09 17:03:49 +0530

    random commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ function App() {
        Now lets work on commit 1
        Now lets work on commit 2
        new branch nigga
+
+       This is git practice and I really want to learn,this.
+       But I don't know why does my git add command is not working, may be this is already added to the git repo.
+       Now lets work on commit 1
+       Now lets work on commit 2
+       new branch nigga
       </header>
     </div>
   );
