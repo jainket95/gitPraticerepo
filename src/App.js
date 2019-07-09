3baa636ae88993a76e8330d1123fae9bfaff999b
thelucidrealist <aniket1995jain@gmail.com>
2019-07-09 16:04:12 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       This is git practice and I really want to learn,this.
+       But I don't know why does my git add command is not working, may be this is already added to the git repo.
+       Now lets work on commit 1
+       Now lets work on commit 2
       </header>
     </div>
   );
